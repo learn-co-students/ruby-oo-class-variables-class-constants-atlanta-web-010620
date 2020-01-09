74d118a539a1fe97cf0b0e8bbd9289cc83614184
tylergreason <tyler.greason@protonmail.com>
2020-01-09 13:58:33 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  # define class constant BRANDS to hold all brands 
+  BRANDS = [] 
+
   def initialize(brand)
     @brand = brand
+    # push @brand to BRANDS constant and remove duplicates
+    BRANDS << @brand
+    BRANDS.uniq!
   end
 
   def cobble
